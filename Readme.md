db904fd6e0b4fecd71b8fbb62295684db25139ee
Christian Moscardi <clm2183@columbia.edu>
2013-06-15 13:57:07 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This was taken from here and we will be building it out to a cool-ass bubble chat application:
+
+http://net.tutsplus.com/tutorials/javascript-ajax/real-time-chat-with-nodejs-socket-io-and-expressjs/
---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var port = 80;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+	res.render("page");
+});
+
+
+var io = require('socket.io').listen(app.listen(port));
+
+var messages = [];
+io.sockets.on('connection', function (socket) {
+	socket.emit('message', { message: 'welcome to the chat' });
+	for(var i=0;i<messages.length;i++){
+		socket.emit('message',{ username:messages[i].username, message: messages[i].message});
+	}
+	socket.on('send', function (data) {
+		messages.push(data);
+		io.sockets.emit('message', data);
+		console.log('message received... '+ data);
+	});
+});
+console.log("Listening on port " + port);
