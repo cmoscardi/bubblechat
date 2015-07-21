5e5584f7d4111944d1b15a835a26fca7e2431165
Christian Moscardi <moscardi79@gmail.com>
2015-07-21 16:49:47 -0700

    moving page.html to index.html for clarity

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var express = require("express");
 var app = express();
-var port = 8080;
+var port = 9999;
 
 app.set('views', __dirname + '/tpl');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
-	res.render("page.html");
-});
-
-app.get("/tree", function(req, res){
-  res.render("tree.html");
+	res.render("index.html");
 });
 
 
@@ -22,13 +18,13 @@ var id = 1;
 io.sockets.on('connection', function (socket) {
   console.log("connection established");
 	for(var i=0;i<messages.length;i++){
-		socket.emit('message',{ parent: messages[i].parent, username:messages[i].username, message: messages[i].message, id: messages[i].id});
+		socket.emit('message', messages[i]);
 	}
 	socket.on('send', function (data) {
     data.id = id++;
 		messages.push(data);
 		io.sockets.emit('message', data);
-		console.log('message received... '+ data);
+		console.log('message received... '+ data.message);
 	});
 });
 console.log("Listening on port " + port);
