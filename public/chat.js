f0d3bd0b75d0b9612aee29f4e35917aac56dff0c
Christian Moscardi <moscardi79@gmail.com>
2015-07-21 16:50:18 -0700

    cleanup of JS logic in preparation for lecture

--- public/chat.js
+++ public/chat.js
@@ -1,44 +1,67 @@
 window.onload = function() {
 
-	var socket = io.connect('http://192.168.0.21:8080');
+  var messages = [];
+  var messagesById = {};
+  var activeMessage = { message: {id: null}};
+
+	var socket = io.connect('http://localhost:9999');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
 	var name = document.getElementById("name");
   var parentNode = document.getElementById("parent");
   var currentID = document.getElementById("currentId");
+  //root nodes for each message tree
 
+  var update = null;
 	socket.on('message', function (data) {
 		if(data.message) {
-			messages.push(data);
-      buildTree();
+      mapMessage(data, activeMessage);
+      if(!update){
+        update = buildTree(messages, activeMessage);
+      }
+      else{
+        update(messages[0]);
+      }
 		} else {
 			console.log("There is a problem:", data);
 		}
 	});
 
 	sendButton.onclick = sendMessage = function() {
+    if(messages.length > 0 && !activeMessage.message.id){
+      alert("Please click a message to reply to first");
+      return;
+    }
 		if(name.value == "") {
 			alert("Please type your name!");
 		} else {
 			var text = field.value;
-			socket.emit('send', { message: text, username: name.value, parent: parseInt(parentNode.value)});
-      buildTree();
-			var html = '';
-			for(var i=0; i<messages.length; i++) {
-				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-				html += messages[i].message + '<br />';
-			}
-			content.innerHTML = html;
-			field.value = "";
+			socket.emit('send', { message: text, username: name.value, parentId: activeMessage.message.id});
 		}
 	};
 
+  function mapMessage(data){
+    data.children = [];
+    messagesById[data.id] = data;
+    if(data.parentId){
+      if(messagesById[data.parentId].children){
+        messagesById[data.parentId].children.push(data);
+      }
+      else {
+        messagesById[data.parentId].children = [data];
+      }
+    }
+    else {
+      messages.push(data);
+    }
+  }
 }
+
+
 $(document).ready(function() {
 	$("#field").keyup(function(e) {
 		if(e.keyCode == 13) {
-      console.log("this should work");
 			sendMessage();
 		}
 	});
--- public/tree.js
+++ public/tree.js
@@ -1,96 +1,145 @@
-var messages = [];
-function lol(){
-  messages.push({id: 4, message: 'lol', parent: 1});
-  buildTree(); 
-}
-
-function buildTree() {
+function buildTree(messages, activeMessage) {
   content.innerHTML = "";
-  var messageMap = messages.reduce(function(map, node) {
-    map[node.id] = node;
-    return map;
-  }, {});
-
-  var treeData = [];
-
-  messages.forEach(function(node) {
-    //add to parent
-    var parent = messageMap[node.parent];
-    if (parent) {
-      (parent.children || (parent.children = []))
-        .push(node);
-    } 
-    else {
-      treeData.push(node);
-    }
-
-  });
 
   var margin = {top: 20, right: 120, bottom: 20, left: 120},
       width = 960 - margin.right - margin.left,
-      height = 500 - margin.top - margin.bottom;
-
-  var i = 0;
+      height = 600 - margin.top - margin.bottom,
+      r = 300;
 
   var tree = d3.layout.tree()
-    .size([height, width]);
-
-  var diagonal = d3.svg.diagonal()
-    .projection(function(d) {
-      return [d.y, d.x];
+    .size([360, r])
+    .separation(separation)
+    .sort(function(a, b){
+      return b.id - a.id;
     });
+
 
   var svg = d3.select("#content").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
+    .style("float", "left")
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top +")");
+    .attr("transform", "translate(" + (r*1.5) + "," + (r) + ")");
 
-  var root = treeData[0]
-
-  update(root);
-
-  function update(source) {
-    nodes = tree.nodes(root).reverse();
-    links = tree.links(nodes);
-
-    nodes.forEach(function(d) { 
-      d.y = d.depth * 180;
-    });
-
-    var node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i);});
+  var messageArea = d3.select("#content").append("div")
+    .style("float", "left")
+    .style("width", "300px")
+    .style("margin-top", "100px")
+    .style('max-width', '300px')
+    .attr('class', 'messageArea');
 
 
-    var nodeEnter = node.enter().append("g")
+  messageArea
+    .append('p')
+    .attr('class', 'username')
+    .attr('color', 'black');
+  messageArea
+    .append('p')
+    .attr('class', 'message')
+    .attr('color', 'black');
+
+
+  var colors = d3.scale.category20();
+  var messageColor = function(d){
+    return d.username;
+  }
+  var source = messages[0];
+  update(source);
+
+  function update(root) {
+    var nodes = tree.nodes(root);
+    var links = tree.links(nodes);
+
+    var link = svg.selectAll(".link")
+      .data(links, function(d){return d.source.id + "-" + d.target.id});
+
+    var linkEnter = link
+      .enter()
+      .append("line")
+      .attr("class", "link")
+      .attr("stroke", "black")
+      .style("stroke-opacity", 1)
+      .style("stroke-width", function(d) { return 1 })
+      .attr("x1", function(d){return rtc(d.source, true).x})
+      .attr("y1", function(d){return rtc(d.source, true).y})
+      .attr("x2", function(d){return rtc(d.source, true).x})
+      .attr("y2", function(d){return rtc(d.source, true).y})
+
+    var node = svg.selectAll(".node")
+      .data(nodes, function(d){return d.id})
+      //keeps them above the links
+      .each(function(){ this.parentNode.appendChild(this);});
+
+    var nodeEnter = node
+      .enter()
+      .append("circle")
       .attr("class", "node")
-      .attr("transform", function(d) {
-        return "translate(" + d.y +","+ d.x +")";
+      .attr("r", function(d) {return (50/(1 + d.depth))})
+      .attr("fill", function(d){return colors(messageColor(d))})
+      .attr("stroke", "black")
+      .attr("transform", function(d) { return "rotate(" + (d.parent && d.parent.x0? d.parent.x0 - 90:0) + ")translate(" + (d.parent && d.parent.y0? d.parent.y0:0) + ")"})
+      .on("mouseover", function(d){
+        messageArea
+          .style('background', colors(messageColor(d)));
+        messageArea.select('p.username')
+          .text("user: " + d.username);
+        messageArea.select('p.message')
+          .text("message: " + d.message);
+      })
+      .on("click", function(d){
+        if(activeMessage.el){
+          activeMessage.el.attr("stroke", "black");
+        }
+        activeMessage.message = d;
+        activeMessage.el = $(this);
+        $(this).attr("stroke", "red");
       });
 
-    nodeEnter.append("circle")
-      .attr("r", 50)
-      .style("fill", "#fff");
+    transitionLinks(link);
+    transitionNodes(node);
+    
+    //set this for the transitions later
+    nodes.forEach(function(n){
+      n.x0 = n.x;
+      n.y0 = n.y;
+    });
+  }
+  return update;
+}
 
-    nodeEnter.append("text")
-      .attr("x", function(d) {
-        return -10;
-      })
-      .attr("dy", 0)
-      .attr("text-anchor", function(d) {
-        return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) { return d.username + ":" + d.message})
-      .style("fill-opacity", 1)
-      svg.selectAll('g.node').on('click', function(n){
-        $("#parent")[0].value = n.id; 
-      });
+function radialToCartesian(d, usex0){
+  if(!d.x){
+    d.x = 90;
+  }
+  if(!d.x0){
+    d.x0 = 90;
+  }
+  var r = d.y;
+  var theta = d.x;
+  if(usex0){
+    r = d.y0;
+    theta = d.x0;
+  }
+  theta= (theta - 90) / 180 * Math.PI;
+  return {x: r*Math.cos(theta), y: r*Math.sin(theta)};
+}
+var rtc = radialToCartesian;
 
-      var link = svg.selectAll("path.link")
-        .data(links, function(d) { return d.target.id;});
+function separation(a, b) {
+  return (a.parent == b.parent ? .5 : 1) / a.depth;
+}
 
-      link.enter().insert("path", "g")
-        .attr("class", "link")
-        .attr("d", diagonal);
-  }  
+function transitionLinks(link){
+  link.transition()
+      .duration(700)
+      .attr("x1", function(d) { return rtc(d.source).x; })
+      .attr("y1", function(d) { return rtc(d.source).y; })
+      .attr("x2", function(d) { return rtc(d.target).x; })
+      .attr("y2", function(d) { return rtc(d.target).y; });
+}
+
+function transitionNodes(node){
+  node.transition()
+      .duration(700)
+      .attr("transform", function(d) { return "rotate(" + (d.x? d.x-90 :0) + ")translate(" + d.y + ")"}); 
 }
